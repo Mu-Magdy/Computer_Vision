7c0253ae5a5aba7850057b7a5648b88832deaf8b
Muhammed Magdy <52422217+Mu-Magdy@users.noreply.github.com>
2021-11-05 16:44:01 +0200

    Adding first project

---
+++ 01hand_track.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+import time
+cap =cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+mpHands=mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw=mp.solutions.drawing_utils
+ptime =0
+ctime=0
+
+while True:
+    success,img=cap.read()
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results=hands.process(imgRGB)
+#    print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id ,lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h,w,c=img.shape
+                cx,cy=int(lm.x*w),int(lm.y*h)
+                print(id,cx,cy)
+                if id ==0:
+                    cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLEd)
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+
+    ctime=time.time()
+    fps=1/(ctime-ptime)
+    ptime=ctime
+
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+
+    cv2.imshow("Image",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
